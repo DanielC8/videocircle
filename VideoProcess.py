351f8dd7ff969a708805d1bf015c9e4a550eaeab
DanielC8 <144239595+DanielC8@users.noreply.github.com>
2024-02-04 20:09:09 -0500

    Update VideoProcess.py

--- VideoProcess.py
+++ VideoProcess.py
@@ -1,12 +1,17 @@
 from FrameProcessing import *
 from FrameOutput import *
+from VideoWriting import *
+
 def VideoProcessing(video):
+    result = initresult("OverlayVideo.mp4",(int(video.get(3)),int(video.get(3))))
     while True:
         ret, frame = video.read()
         if not ret:
             break
         else:
             frame = ImageProcess(frame)
+            writeframe(frame,result)
             OutputFrame("Output", frame)
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
+    return(result)
