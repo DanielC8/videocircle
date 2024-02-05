64bac53b7b6a7c55cef952fc0f7f99339b90f107
DanielC8 <144239595+DanielC8@users.noreply.github.com>
2024-02-04 20:48:04 -0500

    Update VideoWriting.py

--- VideoWriting.py
+++ VideoWriting.py
@@ -1,7 +1,9 @@
 import cv2
 
 def initresult(filename,size):
+    #prepares file to be written
     result = cv2.VideoWriter(filename,cv2.VideoWriter_fourcc(*'mp4v'),10,size)
     return(result)
 def writeframe(frame,result):
+    #writes onto file
     result.write(frame)
