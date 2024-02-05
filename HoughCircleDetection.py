ed915855b0858fa13927eb0d4adfe2b5ce62f165
DanielC8 <144239595+DanielC8@users.noreply.github.com>
2024-02-04 20:46:28 -0500

    Update HoughCircleDetection.py

--- HoughCircleDetection.py
+++ HoughCircleDetection.py
@@ -1,4 +1,5 @@
 import cv2
 
 def HoughCircle(frame,dp,distance,param1,param2,min,max):
+    #hough circle detection
     return(cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, dp, distance, param1=param1, param2=param2, minRadius=min, maxRadius=max))
